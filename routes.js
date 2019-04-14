c0fe85688d0f6b050bada90de92f1315cc22f0be
krushndayshmookh <krushndayshmookh@gmail.com>
2019-04-14 23:08:20 +0530

    fix filename: documents list is now documents

--- routes.js
+++ routes.js
@@ -9,6 +9,10 @@ router.get('/', (req, res) => {
 	res.render('index')
 })
 
+router.get('/documents', (req, res) => {
+	res.render('documents')
+})
+
 router.get('/about', (req, res) => {
 	res.render('about')
 })
